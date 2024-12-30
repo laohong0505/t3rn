5429de80c9324f17a78a994d41aa8e6d296aea45
fdc888 <108032449+laohong0505@users.noreply.github.com>
2024-12-30 11:31:35 +0800

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -1,8 +1,11 @@
+# network_config.py
+
 networks = {
-    'Base Sepolia': {
-        'rpc_url': 'https://sepolia.base.org',  # Base Sepolia 的 RPC 地址
-        'chain_id': 84531,                      # Base Sepolia 的 chain_id
-        'contract_address': '0x30A0155082629940d4bd9Cd41D6EF90876a0F1b5'  # Base 的合约地址
+    'Base': {
+        'rpc_url': 'https://base-sepolia-rpc.publicnode.com', 
+        'chain_id': 84532, 
+        'contract_address': 
+        '0x30A0155082629940d4bd9Cd41D6EF90876a0F1b5' 
     },
     'OP Sepolia': {
         'rpc_url': 'https://endpoints.omniatech.io/v1/op/sepolia/public',
